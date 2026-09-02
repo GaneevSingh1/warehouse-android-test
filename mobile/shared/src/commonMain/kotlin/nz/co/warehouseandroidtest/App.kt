0e3c7781f231448aecc98ce67880c87fcc2848ad
GaneevSingh01 <GaneevSingh1@outlook.com>
2026-09-03 00:04:06 +1200

    made setSingletonImageLoaderFactory compse safe

--- mobile/shared/src/commonMain/kotlin/nz/co/warehouseandroidtest/App.kt
+++ mobile/shared/src/commonMain/kotlin/nz/co/warehouseandroidtest/App.kt
@@ -1,9 +1,11 @@
 package nz.co.warehouseandroidtest
 
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import androidx.compose.ui.tooling.preview.Preview
 import coil3.ImageLoader
-import coil3.compose.setSingletonImageLoaderFactory
+import coil3.PlatformContext
+import coil3.SingletonImageLoader
 import coil3.network.ktor3.KtorNetworkFetcherFactory
 import nz.co.warehouseandroidtest.ui.dashboard.DashboardScreen
 import nz.co.warehouseandroidtest.ui.dashboard.DashboardViewModel
@@ -12,17 +14,18 @@ import nz.co.warehouseandroidtest.ui.theme.WarehouseTheme
 
 @Composable
 fun App() {
-    setSingletonImageLoaderFactory { context ->
-        ImageLoader.Builder(context)
-            .components {
-                add(KtorNetworkFetcherFactory())
-            }
-            .build()
-    }
+    remember { SingletonImageLoader.setSafe(::createImageLoader) }
     WarehouseTheme {
         AppNavHost()
     }
 }
+
+private fun createImageLoader(context: PlatformContext): ImageLoader =
+    ImageLoader.Builder(context)
+        .components {
+            add(KtorNetworkFetcherFactory())
+        }
+        .build()
 
 @Preview
 @Composable
